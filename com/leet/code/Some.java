f712c3da30dc0ab1ad35cd376e5c884a120578bf
guoyanrui <wb_guoyanrui@kuaishou.com>
2021-12-27 18:24:09 +0800

    data

--- com/leet/code/Some.java
+++ com/leet/code/Some.java
@@ -1,37 +1,54 @@
 package leet.code;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author guoyanrui on 2021/12/24
  */
 public class Some {
     //快排
-    public static void paixu(int[] nums,int left,int right){
-        if(left>right)return;
-        int i=left;
-        int j=right;
+    public static void paixu(int[] nums, int left, int right) {
+        if (left > right) return;
+        int i = left;
+        int j = right;
         int base = nums[i];
-        while(i!=j){
-            while(nums[j]>=base&&i<j){
+        while (i != j) {
+            while (nums[j] >= base && i < j) {
                 j--;
             }
-            while(nums[i]<=base&&i<j){
+            while (nums[i] <= base && i < j) {
                 i++;
             }
             int temp = nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
+            nums[i] = nums[j];
+            nums[j] = temp;
         }
-        nums[left]=nums[i];
-        nums[i]=base;
-        paixu(nums,left,i-1);
-        paixu(nums,i+1,right);
+        nums[left] = nums[i];
+        nums[i] = base;
+        paixu(nums, left, i - 1);
+        paixu(nums, i + 1, right);
+    }
+
+    //拿点数据
+    @Test
+    public static void getData() throws IOException {
+        File file = new File("data\\test.csv");
+        Reader reader = new FileReader(file);
+        char[] ch = new char[reader.read()];
+        while (reader.read(ch) != -1) {
+            System.out.println(new String(ch));
+        }
     }
 
     public static void main(String[] args) {
-        int[] nums={21,43,3,657,67,8,32,8,9,9765,654,43436};
-        paixu(nums,0,nums.length-1);
+        int[] nums = {21, 43, 3, 657, 67, 8, 32, 8, 9, 9765, 654, 43436};
+        paixu(nums, 0, nums.length - 1);
         System.out.println(Arrays.toString(nums));
     }
 }
