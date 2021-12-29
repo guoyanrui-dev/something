533e39b30b9f81753cc46244bbb80bd8cf47270a
guoyanrui <1178864514@qq.com>
2021-12-30 00:02:46 +0800

    add sort

---
+++ com/leet/code/Sort.java
@@ -0,0 +1,79 @@
+package leet.code;
+
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class Sort {
+    //快速排序测试
+    public static void quiklySort(int[] nums, int left, int right) {
+        if (left > right) return;
+        int i = left;
+        int j = right;
+        int base = nums[i];
+        while (i != j) {
+            while (nums[j] >= base && i < j) {
+                j--;
+            }
+            while (nums[i] <= base && i < j) {
+                i++;
+            }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        nums[left] = nums[i];
+        nums[i] = base;
+        quiklySort(nums, left, i - 1);
+        quiklySort(nums, i + 1, right);
+    }
+
+    //冒泡
+    public static void bubbleSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j + 1] < nums[j]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    //选择排序
+    public static void selectSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[min]) {
+                    min = j;
+                }
+            }
+            if (i != min) {
+                int temp = nums[i];
+                nums[i] = nums[min];
+                nums[min] = temp;
+            }
+        }
+    }
+
+    //jiujiu
+    @Test
+    public static void jiujiu() {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + "*" + i + "=" + j * i + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {32, 12, 67, 1, 23, 5, 7, 7876, 42233, 432423, 66};
+        quiklySort(nums, 0, nums.length - 1);
+//        selectSort(nums);
+//        bubbleSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
