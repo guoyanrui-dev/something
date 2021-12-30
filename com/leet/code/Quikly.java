963b51e8d3a30300b17013e1fc7f054c770689ce
guoyanrui <wb_guoyanrui@kuaishou.com>
2021-12-30 10:30:01 +0800

    1230

--- com/leet/code/Quikly.java
+++ com/leet/code/Quikly.java
@@ -76,11 +76,29 @@ public class Quikly {
         }
     }
 
+    //选择排序
+    public static void selectSot(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                int temp = nums[i];
+                nums[i] = nums[min];
+                nums[min] = temp;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums = {1, 543, 32, 32, 12, 2, 3, 4, 5, 678998, 788};
 //        bobble(nums);
 //        quiklySort(nums, 0, nums.length - 1);
-        selectSort(nums);
+//        selectSort(nums);
+        selectSot(nums);
         System.out.println(Arrays.toString(nums));
     }
 }
