7dda6927e045cab3de0bd43477a8e6d94329af19
guoyanrui <1178864514@qq.com>
2022-01-01 14:50:06 +0800

    2022

---
+++ com/leet/code/NewYear.java
@@ -0,0 +1,67 @@
+package leet.code;
+
+
+import java.util.Arrays;
+
+public class NewYear {
+    //maopao
+    public static void bub(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j + 1] < nums[j]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    //xuanze
+    public static void sele(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                int temp = nums[i];
+                nums[i] = nums[min];
+                nums[min] = temp;
+            }
+        }
+    }
+
+    //kuai
+    public static void qui(int[] nums, int left, int right) {
+        if (left > right) return;
+        int i = left;
+        int j = right;
+        int base = nums[i];
+        while (i != j) {
+            while (nums[j] >= base && i < j) {
+                j--;
+            }
+            while (nums[i] <= base && i < j) {
+                i++;
+            }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        nums[left] = nums[i];
+        nums[i] = base;
+        qui(nums, left, i - 1);
+        qui(nums, i + 1, right);
+    }
+
+    public static void main(String[] args) {
+        int[] array={1,321,5,765,234,8,768,35412,47,6798679,4325643};
+        sele(array);
+//        qui(array,0,array.length-1);
+//        bub(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
