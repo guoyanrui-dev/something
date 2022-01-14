7bdeb54e858cba512919bd83e7c05310819f62b1
guoyanrui <wb_guoyanrui@kuaishou.com>
2022-01-14 16:38:53 +0800

    add sort

---
+++ com/leet/code/self/ThreeSort.java
@@ -0,0 +1,69 @@
+package leet.code.self;
+
+import java.util.Arrays;
+
+/**
+ * @author guoyanrui on 2022/1/14
+ */
+public class ThreeSort {
+    //kuai
+    public static void quikly(int[] nums, int left, int right) {
+        if (left > right) return;
+        int i = left;
+        int j = right;
+        int base = nums[i];
+        while (i != j) {
+            while (nums[j] >= base && i < j) {
+                j--;
+            }
+            while (nums[i] <= base && i < j) {
+                i++;
+            }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        nums[left] = nums[i];
+        nums[i] = base;
+        quikly(nums, left, i - 1);
+        quikly(nums, i + 1, right);
+    }
+
+    //xuanze
+    public static void select(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                int temp = nums[min];
+                nums[min] = nums[i];
+                nums[i] = temp;
+            }
+        }
+    }
+
+    //maopao
+    public static void bubble(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j + 1] < nums[j]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {43, 6534, 123, 45, 7, 8, 9, 4214, 76998, 5654, 32433};
+//        quikly(array, 0, array.length - 1);
+//        select(array);
+        bubble(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
