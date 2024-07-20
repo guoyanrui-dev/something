183366dfa04db58660e9243d31d4a0facb38cecb
guoyanrui <1178864514@qq.com>
2024-07-20 22:24:47 +0800

    fix

--- com/leet/code/Quikly.java
+++ com/leet/code/Quikly.java
@@ -66,7 +66,7 @@ public class Quikly {
     }
 
     //输出九九乘法表
-    @Test
+    @org.testng.annotations.AfterTest
     public static void jiu() {
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= i; j++) {
